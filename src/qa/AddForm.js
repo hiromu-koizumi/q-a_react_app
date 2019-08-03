70a1ff94c5c82e28ec5921d0f9f9c026c115b12d
hiromu-koizumi <abn21029@gmail.com>
2019-08-03 16:55:06 +0900

    add:firebase

---
+++ src/qa/AddForm.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addQa } from './Store';
+
+class AddForm extends Component{
+    constructor(props) {
+        super(props);
+
+        //stateの値を再定義しているのは、ここで値の変更を出来るようにするため。
+        //stateをpropsで受け取り値を変更することは出来ない。
+        this.state = {
+            user: '',
+            title: '',
+            question: ''
+        };
+    this.question = this.question.bind(this);
+    this.userName = this.userName.bind(this);
+    this.title = this.title.bind(this);
+    this.doAction = this.doAction.bind(this);
+    }
+
+
+    // テキストボックスの中身が変更されたら呼び出される
+  question(e) {
+    this.setState({ question: e.target.value });
+  }
+  title(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  userName(e){
+    this.setState({ user: e.target.value});
+  }
+    
+    
+    doAction(e) {
+        e.preventDefault();
+        let action = addQa(this.state.user, this.state.title, this.state.question);
+        this.props.dispatch(action);
+
+        this.setState({
+            user: '',
+            title: '',
+            question: ''
+        });
+    }
+
+    render() {
+        return (
+            <div>
+            <header className="">Q&A</header>
+            <div className="ui container">
+                <form onSubmit={this.doAction}
+                    autoComplete="off"
+                    className="ui form"
+                >
+                    <div className="field">
+                        <input
+                            onChange={this.userName}
+                            value={this.state.user}
+                            id='tweets'
+                            placeholder="名前"
+                        />
+                    </div>
+
+                    <div className="field">
+                        <input
+                            onChange={this.title}
+                            value={this.state.title}
+                            id='tweets'
+                            placeholder="タイトル"
+                        />
+                    </div>
+
+                    <div className="field">
+                        <textarea
+                            id="new-todo"
+                            onChange={this.question}
+                            value={this.state.question}
+                            placeholder="質問"
+                            rows="3"
+                        />
+                    </div>
+                    <button
+                        className="ui green inverted button"
+                        type="submit"
+                    >質問する
+            </button>
+                </form>
+                </div>
+                </div>
+        )
+    }
+}
+
+export default connect()(AddForm);
---
+++ src/qa/Store.js
@@ -0,0 +1,68 @@
+import { createStore } from 'redux';
+
+import firebase from 'firebase/app';
+import firestore from 'firebase/firestore';
+import config from '../firebase-config.js';
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+const collection = db.collection('tweets');
+
+const initData = {
+    data: [],
+    user: '',
+    title: '',
+    question: ''
+};
+
+//レデューサー
+export function qaReducer(state = initData, action) {
+    switch (action.type) {
+        case 'ADD':
+            return addReduce(state, action);
+        default:
+            return state;
+    }
+}
+
+//レデュースアクション
+//質問投稿レデュース処理
+function addReduce(state, action) {
+    let data = {
+    user:action.user,
+    title:action.title,
+    question: action.question,
+    }
+    let newdata = state.data.slice();
+    newdata.unshift(data);
+
+        //データベースに保存
+    collection.add({
+      user: action.user,
+      title: action.title,
+      question: action.question,
+      created:
+        firebase.firestore.FieldValue.serverTimestamp()
+    }).then(doc => {
+      console.log(`${doc.id}をDBに保存した`);
+    })
+      .catch(error => {
+        console.log(error);
+      });
+    
+    return {
+        data: newdata,
+    }
+}
+
+//アクションクリエーター(リデューサーに処理を渡している)
+export function addQa(user, title, question) {
+    return {
+        type: 'ADD',
+        user: user,
+        title: title,
+        question,question
+    }
+}
+
+export default createStore(qaReducer);
