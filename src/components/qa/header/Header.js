02ccc9d449bcb98f71f707fb333fbde8e0d298e4
小泉大夢 <abn21029@gmail.com>
2020-10-15 22:52:34 +0900

    fix_logo_style

--- src/components/qa/header/Header.js
+++ src/components/qa/header/Header.js
@@ -23,7 +23,7 @@ const Header = ({isSignedIn}) => {
         // </div>
         <div className="header-wrap">
             <svg xmlns="http://www.w3.org/2000/svg" width="105" height="51" viewBox="0 0 45 21">
-            <text id="キュー" transform="translate(0 16)" fill="#e18992" font-size="15" font-family="Mplus1p-ExtraBold, 'Mplus 1p'" font-weight="800"><tspan x="0" y="0">キュー</tspan></text>
+            <text id="キュー" transform="translate(0 16)" fill="#e18992" fontSize="15" fontFamily="Mplus1p-ExtraBold, 'Mplus 1p'" fontWeight="800"><tspan x="0" y="0">キュー</tspan></text>
             </svg>
         </div>
     )
